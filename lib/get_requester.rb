26be9726d8ba5a97f28abf3f13c7da42f006a599
korey ko <koreykendrick1@gmail.com>
2020-06-20 19:54:27 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
 # Write your code here
+
+ 
+class Get_Reqester 
+ 
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+ 
+  def get_programs
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+ 
+end
+
+
+
+
+  
